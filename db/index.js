2de4637314fbc23dd22c04fd415f73ab35024e44
Patrick Lorden <pjlorden@gmail.com>
2021-12-02 20:59:39 -0600

    implemented reviews and review meta queries

--- db/index.js
+++ db/index.js
@@ -1,22 +1,105 @@
 const { Pool } = require('pg');
 const config = require('./postgres_config');
 
-const getReviews = (product_id, count, page, sort) => {
+const db = new Pool(config);
 
+// GET REVIEWS ENDPOINT METHODS
+
+const getReviews = async (product_id, count, page, sort) => {
+  const sortTypes = {
+    relevent: 'helpfulness',
+    helpful: 'helpfulness',
+    newest: 'date'
+  }
+
+  const queryString = 'SELECT id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness FROM reviews WHERE product_id = $1 ORDER BY $2 desc LIMIT $3 OFFSET $4';
+  const values = [product_id, sortTypes.sort, count, (count * page - count)];
+
+  return db
+    .query(queryString, values)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error(err.stack)
+    })
 }
 
-const getMetaData = (id) => {
+const getPhotos = (review_id) => {
+  const queryString = 'select id, url FROM photos WHERE review_id = $1'
+  const values = [review_id]
 
+  return db
+    .query(queryString, values)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err.stack)
+    })
 }
+
+// GET META DATA ENDPOINT METHODS
+
+const getMetaRatings = (review_id) => {
+  const queryString = 'SELECT rating, count(rating) FROM reviews WHERE product_id=$1 GROUP BY rating'
+  const value = [review_id]
+
+  return db
+    .query(queryString, value)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => console.error(err.stack))
+}
+
+const getMetaRecommended = (review_id) => {
+  const queryString = 'SELECT recommend, count(recommend) FROM reviews WHERE product_id=$1 GROUP BY recommend';
+  const value = [review_id]
+
+  return db
+    .query(queryString, value)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => console.error(err.stack))
+}
+
+const getCharacteristics = (review_id) => {
+  const queryString = 'SELECT characteristics.name, AVG(characteristics_reviews.value), characteristics.name FROM characteristics FULL JOIN characteristics_reviews ON characteristics_reviews.characteristic_id = characteristics.id WHERE characteristics.product_id=$1 GROUP BY characteristics.name;';
+  const value = [review_id]
+
+  return db
+  .query(queryString, value)
+  .then((res) => {
+    return res.rows;
+  })
+  .catch((err) => console.error(err.stack))
+}
+
+// POST REVIEWS METHODS
 
 const addReview = (reviewObj) => {
 
 }
 
+const addCharacteristics = (reviewObj) => {
+
+}
+
+// UPDATE HELPFUL AND REPORT MEHTODS
 const helpful = (id, direction) => {
 
 }
 
 const reported = (id) => {
 
+}
+
+module.exports = {
+  getReviews: getReviews,
+  getPhotos: getPhotos,
+  getMetaRatings: getMetaRatings,
+  getMetaRecommended: getMetaRecommended,
+  getCharacteristics:getCharacteristics
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const db = require('../db')
 const port = 3000;
 
 app.use(express.json());
 
-app.get('/reviews', (req, res) => {
-
+app.get('/reviews', async (req, res) => {
+  const params = req.query;
+  let test = await db.getReviews(params.product_id, params.count, params.page, params.sort);
+  // console.log(test);
+  for (var i = 0; i < test.length; i++) {
+    let photos = await db.getPhotos(test[i].id)
+    test[i].photos = photos;
+  }
+  let result = {
+    product: params.product_id,
+    page: params.page,
+    count: params.count,
+    results: test
+  }
+  res.send(result);
 })
 
-app.get('/reviews/meta', (req, res) => {
+app.get('/reviews/meta', async (req, res) => {
+  const id = req.query.product_id;
 
+  let ratings = await db.getMetaRatings(id);
+  let recommended = await db.getMetaRecommended(id);
+  let characteristics = await db.getCharacteristics(id);
+  res.send('test');
 })
 
 app.post('/reviews', (req, res) => {
